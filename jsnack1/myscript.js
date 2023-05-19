836a7ee325eda865b03503b005f75cf10ec7916c
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-05-19 16:35:38 +0200

    jsnack1 completed

--- jsnack1/myscript.js
+++ jsnack1/myscript.js
@@ -1,1 +1,18 @@
 /* Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50. */
+
+const numberList = [];
+let sum = 0;
+let i = 0;
+
+while (sum < 50) {
+    let number = parseInt(prompt("Inserisci un numero"));
+
+    while (isNaN(number)) {
+        number = parseInt(prompt("Inserisci un numero"));
+    }
+    
+    numberList.push(number);
+    sum += number;
+}
+
+console.log(numberList, sum);
