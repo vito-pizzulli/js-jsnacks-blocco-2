6dd9da0f5b6f6485871c35594868eef6467b8198
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-05-19 16:28:34 +0200

    init index.html and myscript.js

---
+++ jsnack1/myscript.js
@@ -0,0 +1,1 @@
+/* Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50. */
