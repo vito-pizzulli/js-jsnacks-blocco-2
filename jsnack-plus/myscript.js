8a3aabf51c81df2f894aab8b903b574f38006195
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-05-23 12:26:35 +0200

    bonus completed

--- jsnack-plus/myscript.js
+++ jsnack-plus/myscript.js
@@ -1,3 +1,5 @@
+
+
 function createLi () {
     const liElement = document.createElement('li');
     liElement.innerHTML = "Contenuto";
@@ -8,5 +10,10 @@ const ulElement = document.querySelector('ul');
 
 for (let i = 0; i < 10; i++) {
     const li = createLi();
+
+    li.addEventListener('click', function () {
+        li.classList.toggle('txt-line-through');
+    })
+
     ulElement.appendChild(li);
 }
---
+++ jsnack-plus/readme.md
@@ -0,0 +1,6 @@
+<h1>Jsnack Plus</h1>
+
+Generare una lista usando una funzione.<br>
+ Nell'html ci dovrà essere esclusivamente un elemento ul parent, attraverso una funzione creiamo un singolo elemento html di tipo li, poi ne aggiungiamo una decina all'ul, sempre via js.
+
+<strong>BONUS: Se clicchiamo sul li il suo testo verrà sbarrato.</strong>
